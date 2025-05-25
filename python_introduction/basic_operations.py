72e80d6a03fa36f4b9348a0ffaa11ad0efa0e560
Bamlak <bamlakbee.12@gmail.com>
2025-05-25 17:02:05 +0300

    Add basic_operation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 10
+number2 = 5
+
+
+addition=number1+number2
+subtraction=number1-number2
+multiplication=number1*number2
+
+result1 = "Addition of" + str(number1)+ "and" + str(number2) + "is" + str(addition)
+result2 = "Subtraction of" + str(number1)+ "and" + str(number2) + "is" + str(subtraction)
+result3 = "Multiplication of" + str(number1)+ "and" + str(number2) + "is" + str(multiplication)
+print(result1, result2,result3)
