dda953cf4a8a4dd01c4e13d176006a2641cac2c4
Bamlak <bamlakbee.12@gmail.com>
2025-05-29 20:10:12 +0300

    Modified pattern drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,10 @@
-size =int(input("Enter the size of the pattern: "))
-for i in range(size):
-    for j in range(size):
+size = int(input("Enter the size of the pattern: "))
+
+i = 0
+while i < size:
+    j = 0
+    while j < size:
         print("x", end="")
+        j += 1
     print()
+    i += 1
