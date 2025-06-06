220116a6b553ef29fd14a308a4a396684e809a68
Bamlak <bamlakbee.12@gmail.com>
2025-06-06 23:57:55 +0300

    modified_arthimetic_operations

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,26 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return num1+num2
-        case "subtract":
-            return num1-num2
-        case "divide":
+    if operation == "add":
+        return num1+num2
+    elif operation == "subtract":
+        return num1-num2
+    elif operation == "multiply":
+        return num1*num2
+    elif operation == "divide":
+        if num2 == 0:
+            print("Cannot divide by zero")
+        elif num2 > 0:
             return num1/num2
-        case "multiply":
-            return num1*num2
+    else:
+        return 
+    # match operation:
+    #     case "add":
+    #         return num1+num2
+    #     case "subtract":
+    #         return num1-num2
+    #     case "divide":
+    #         return num1/num2
+    #     case "multiply":
+    #         return num1*num2
     
 
- 
- 
+  
