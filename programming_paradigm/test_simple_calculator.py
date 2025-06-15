cd20e7833fea093b2a742d9e59d5c091d7442dd3
Bamlak <bamlakbee.12@gmail.com>
2025-06-15 20:13:37 +0300

    Modifiedn test_simple_cal

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,30 +7,30 @@ class TextClass (unittest.TestCase):
     
     def setUp(self):
         # Set up a SimpleCalculator instance before each test
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
 
     def test_addition(self):
         """Test the add method."""
-        self.assertEqual(self.calculator.add(2, 3), 5)
-        self.assertEqual(self.calculator.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
 
     def test_subtraction(self):
         """Test the subtract method."""
-        self.assertEqual(self.calculator.subtract(5, 3), 2)
-        self.assertEqual(self.calculator.subtract(3, 5), -2)
-        self.assertEqual(self.calculator.subtract(-3, -5), -2)
-        self.assertEqual(self.calculator.subtract(3, -5), 8)
-        self.assertEqual(self.calculator.subtract(-3, 5), -8)
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(3, 5), -2)
+        self.assertEqual(self.calc.subtract(-3, -5), -2)
+        self.assertEqual(self.calc.subtract(3, -5), 8)
+        self.assertEqual(self.calc.subtract(-3, 5), -8)
 
     def test_multiply(self):
         """Test the multiply method."""
-        self.assertEqual(self.calculator.multiply(2, 3), 6)
-        self.assertEqual(self.calculator.multiply(-1, 5), -5)
-        self.assertEqual(self.calculator.multiply(-1, 0), 0)
-        self.assertEqual(self.calculator.multiply(1, 0), 0)
-        self.assertEqual(self.calculator.multiply(1, -8), )
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-1, 5), -5)
+        self.assertEqual(self.calc.multiply(-1, 0), 0)
+        self.assertEqual(self.calc.multiply(1, 0), 0)
+        self.assertEqual(self.calc.multiply(1, -8), )
 
     def test_divide(self):
         """Test the divide method."""
-        self.assertEqual(self.calculator.divide(6, 3), 2)
-        self.assertIsNone(self.calculator.divide(5, 0))
+        self.assertEqual(self.calc.divide(6, 3), 2)
+        self.assertIsNone(self.calc.divide(5, 0))
