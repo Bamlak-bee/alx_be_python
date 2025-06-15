1fb4a2cf78c7cc513243da6336416e5b422c9220
Bamlak <bamlakbee.12@gmail.com>
2025-06-15 23:09:55 +0300

    Changed bankaccount

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,10 +1,10 @@
 class BankAccount:
     def __init__(self,account_balance,initial_balance=0):
         self.account_balance = account_balance
-        self.intial_balance = initial_balance
+        # self.intial_balance = initial_balance
         
     def deposit(self,amount):
-        self.intial_balance += amount    
+        self.account_balance += amount    
         
     def withdraw(self,amount):
         if self.account_balance < amount:
@@ -14,7 +14,7 @@ class BankAccount:
         
         
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:2f}")
 
 
 
