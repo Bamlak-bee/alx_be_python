c025ee13fc17ef7de80e4d1c4822616197a67e37
Bamlak <bamlakbee.12@gmail.com>
2025-06-15 13:46:05 +0300

    Adding bankaccount

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__(self,account_balance):
+        self.accountnce = account_balance
+        
+    def deposit(amount):
+        account_balance += amount    
+        
+    def withdraw(amount):
+        if account_balance < amount:
+            return False
+        else:
+            return True
+        
+        
+    def display_balance():
+        print(f"Your balance is: {account_balance}")
+
