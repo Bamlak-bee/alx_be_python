f9becf414947fc8b489e33a9f077cc9379f23705
Bamlak <bamlakbee.12@gmail.com>
2025-06-15 22:47:47 +0300

    Modified division_cal

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -5,7 +5,7 @@ def safe_divide(numerator, denominator):
         denominator = float(denominator)
         
         result = numerator/denominator
-        print(f"The result of the division is")
+        # print(f"The result of the division is {result}")
         
         return result
     except ValueError:
