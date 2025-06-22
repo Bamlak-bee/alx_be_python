4792af7368294ec32843a27c8aa65973a555bb3a
Bamlak <bamlakbee.12@gmail.com>
2025-06-22 18:03:24 +0300

    Modified library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -12,7 +12,7 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
     def __str__(self):
-        return f'EBook: "{self.title}" by {self.author}, {self.file_size}KB'
+        return f'EBook: {self.title} by {self.author}, File Size: {self.file_size}KB'
 
 
 class PrintBook(Book):
@@ -20,7 +20,7 @@ class PrintBook(Book):
         super().__init__(title,author)
         self.page_count = page_count
     def __str__(self):
-        return f'PrintBook: "{self.title}" by {self.author}, Page Count:{self.page_count}'
+        return f'PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}'
 
         
 class Library:
